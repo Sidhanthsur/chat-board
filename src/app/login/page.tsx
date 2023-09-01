976bc6aa6787c7d731df1926c42511a456946670
Sidhanth surana <sidhanth.surana12@gmail.com>
2023-09-01 16:19:33 +0530

    safe

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,16 +2,20 @@ import './login.css'
 import Image from 'next/image'
 const Login = () => {
     return (
-        <div className="">
+        <div className="login-container">
             <Image
                 width={40}
                 height={40}
                 alt="logo"
-                src="/logo.png" />
+                src="/logo.png"
+                className='mb-8' />
 
             <div className='login-container__box'>
-                <div className='login-container__box-title '>
+                <div className='login-container__box-title mb-1'>
                     Welcome Back
+                </div>
+                <div className='login-container__box-subtitle'>
+                    Log into your account
                 </div>
             </div>
         </div>
