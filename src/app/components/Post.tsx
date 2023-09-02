dcf434169d7358c1f83b316d37af9df5b0c039c5
Sidhanth surana <sidhanth.surana12@gmail.com>
2023-09-02 11:09:29 +0530

    safe

--- src/app/components/Post.tsx
+++ src/app/components/Post.tsx
@@ -1,10 +1,38 @@
 import './styles/Post.css'
 import { Post } from '../types/Post'
+import Image from 'next/image'
 
-export default function Post({ profileImage, author }: Post) {
+export default function Post({ profileImage, author, time }: Post) {
     return (
-        <div>
-            {author}
+        <div className='post p-6'>
+
+            <div className='flex justify-between'>
+                <div className='flex'>
+                    {/** Author details */}
+                    <Image
+                        src={profileImage}
+                        alt={author}
+                        width={45}
+                        height={45}
+                        className='post__profile'
+                    />
+                    <div className='ml-4'>
+                        <div className='post__author-name'>{author}</div>
+                        <div className='post__time'>{time}</div>
+                    </div>
+
+
+                </div>
+
+                <Image
+                    width={20}
+                    height={20}
+                    alt="dots"
+                    src="/dots.svg" />
+            </div>
+
+
+
         </div>
     )
 }
